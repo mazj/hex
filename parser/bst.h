671043cc6b7dc79b88ddc176b0c5f66e359cc22e
Yanzheng Li <yanzheng819@gmail.com>
2012-02-20 01:45:28 -0800

    02/20/2012 - change to bst_apend_to_list and bst_to_list in bst.h

--- parser/bst.h
+++ parser/bst.h
@@ -66,24 +66,26 @@ void* bst_back(const BinaryNode* node) {
 	return (node->right == 0) ? node->value : bst_back(node->right) ? bst_back(node->right) : node->value;
 }
 
-static void bst_append_to_list(BinaryNode* node, Node* list) {
+static void bst_append_to_list(BinaryNode* node, Node** list) {
     if(!node) return;
-    bst_append_to_list(node->left, list);
-    if(node->value) {
-        Node* tmp = (Node*)malloc(sizeof(Node));
-        memset(tmp->value, list->value, sizeof(list->value));
-        list->next = tmp;
-        tmp->next = 0;
-    } else return;
     bst_append_to_list(node->right, list);
+    if(node && node->value) {
+        Node* tmp = (Node*)malloc(sizeof(Node));
+        tmp->value = node->value; //memset(tmp->value, list->value, sizeof(list->value));
+        tmp->next = *list;
+        *list = tmp;
+        // return list;
+    }
+    bst_append_to_list(node->left, list);
+    // return list;
 }
 
 /* Generates a singly linked list of all
    the values contained by the given node
    and its children in ascending order. */
 Node* bst_to_list(BinaryNode* node) {
-    Node* list = (Node*)malloc(sizeof(Node));
-    bst_append_to_list(node, list);
+    Node* list = 0;
+    bst_append_to_list(node, &list);
     return list;
 }
 
