385615d850f6fd886f866015c8b2cab45611b6d4
Yanzheng Li <yanzheng819@gmail.com>
2012-02-20 01:00:00 -0800

    02/20/2012 - added test macro to unittest.h

--- parser/unittest.h
+++ parser/unittest.h
@@ -1,7 +1,26 @@
 #ifndef _UNIT_TEST_H_
 #define _UNIT_TEST_H_
 
+#include <stdio.h>
 #include "assert.h"
 #include "time.h"
 
 #endif // _UNIT_TEST_H_
+
+#ifndef TEST_INIT(x)
+#define TEST_INIT(x) 					\
+		printf("\nUNIT TEST: %s\n", (x));	\
+		int i = 0;
+#endif
+
+#ifndef TEST_CHECK()
+#define TEST_CHECK()					\
+		i++;							\
+		printf("TEST[%d]: PASS\n", i);	\
+		sleep(1);
+#endif
+
+#ifndef TEST_END()
+#define TEST_END()						\
+		printf("TEST SUCCESSFUL...\n");
+#endif
