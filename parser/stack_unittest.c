4751c86041e0367491fbe4c44b87e572ca0cb548
Yanzheng Li <yanzheng819@gmail.com>
2012-02-20 01:00:55 -0800

    02/20/2012 - changed to using test macro from unittest.h

--- parser/stack_unittest.c
+++ parser/stack_unittest.c
@@ -3,68 +3,56 @@
 
 int main(int argc, char const *argv[])
 {
-	int i = 0;
+	TEST_INIT(__FILE__);
 
 	Stack *s = 0;
 	assert(s == 0);
 	assert(stack_size(s) == 0);
 	assert(stack_top(s) == 0);
-	i++;
-	printf("TEST[%d]: PASS\n", i);	// TEST 1
-	sleep(1);
+	TEST_CHECK();
 
 	s = (Stack*)malloc(sizeof(Stack));
 	assert(s != 0);
 	assert(stack_size(s) == 0);
 	assert(stack_top(s) == 0);
-	i++;
-	printf("TEST[%d]: PASS\n", i);	// TEST 2
-	sleep(1);
+	TEST_CHECK();
 
 	stack_push(s, "1");
 	assert(s != 0);
 	assert(stack_size(s) == 1);
 	assert(stack_top(s) == "1");
-	i++;
-	printf("TEST[%d]: PASS\n", i);	// TEST 3
-	sleep(1);
+	TEST_CHECK();
 
 	stack_push(s, "2");
 	assert(s != 0);
 	assert(stack_size(s) == 2);
 	assert(stack_top(s) == "2");
-	i++;
-	printf("TEST[%d]: PASS\n", i);	// TEST 4
-	sleep(1);
+	TEST_CHECK();
 
 	char *v = stack_pop(s);
 	assert(s != 0);
 	assert(stack_size(s) == 1);
 	assert(stack_top(s) == "1");
 	assert(v == "2");
-	i++;
-	printf("TEST[%d]: PASS\n", i);	// TEST 5
-	sleep(1);
+	TEST_CHECK();
 
 	v = stack_pop(s);
 	assert(s != 0);
 	assert(stack_size(s) == 0);
 	assert(stack_top(s) == 0);
 	assert(v == "1");
-	i++;
-	printf("TEST[%d]: PASS\n", i);	// TEST 6
-	sleep(1);
+	TEST_CHECK();
 
 	v = stack_pop(s);
 	assert(s != 0);
 	assert(stack_size(s) == 0);
 	assert(stack_top(s) == 0);
 	assert(v == 0);
-	i++;
-	printf("TEST[%d]: PASS\n", i);	// TEST 6
-	sleep(1);
+	TEST_CHECK();
+
+	free(s);
 
 	// End of test.
-	printf("TEST SUCCESSFUL...\n");
+	TEST_END();
 	return 0;
 }
