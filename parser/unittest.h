ad2d564d86c2ae9b2431574d65da453603f7d1ed
Yanzheng Li <yanzheng819@gmail.com>
2012-02-20 01:02:52 -0800

    02/20/2012 - fix to unittest.h

--- parser/unittest.h
+++ parser/unittest.h
@@ -1,3 +1,4 @@
+/* Unit test macros. */
 #ifndef _UNIT_TEST_H_
 #define _UNIT_TEST_H_
 
@@ -5,22 +6,24 @@
 #include "assert.h"
 #include "time.h"
 
-#endif // _UNIT_TEST_H_
+
 
 #ifndef TEST_INIT(x)
-#define TEST_INIT(x) 					\
-		printf("\nUNIT TEST: %s\n", (x));	\
+#define TEST_INIT(x) 					  \
+		printf("\nUNIT TEST: %s\n", (x)); \
 		int i = 0;
-#endif
+#endif // TEST_INIT(x)
 
 #ifndef TEST_CHECK()
 #define TEST_CHECK()					\
 		i++;							\
 		printf("TEST[%d]: PASS\n", i);	\
 		sleep(1);
-#endif
+#endif  // TEST_CHECK()
 
 #ifndef TEST_END()
 #define TEST_END()						\
 		printf("TEST SUCCESSFUL...\n");
-#endif
+#endif  // TEST_END()
+
+#endif // _UNIT_TEST_H_
