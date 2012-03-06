256615ba315a52cfd8b6e1bf301bade9e8a082b0
Yanzheng Li <yanzheng819@gmail.com>
2012-03-05 22:47:33 -0800

    03/05/2012 - added MALLOC and DEREF_VOID utility macros in parser/utils.h

--- parser/utils.h
+++ parser/utils.h
@@ -6,6 +6,7 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
+
 //===========================================================================
 // Singly linked list node.
 //===========================================================================
@@ -15,6 +16,7 @@ typedef struct {
 	struct Node* next;
 } Node;
 #endif // _NODE_H_
+
 
 //===========================================================================
 // Doubly linked list node.
@@ -27,5 +29,32 @@ typedef struct {
 } DoubleNode;
 #endif // _DOUBLE_NODE_H_
 
+
+//===========================================================================
+// malloc
+//===========================================================================
+#define MALLOC(T)					\
+	(T*)malloc(sizeof(T))
+
+
+//===========================================================================
+// Derefernece a void* pointer to a data of specified type.
+//
+// T: The specified data type to be dereferenced.
+// v: the value of type void*
+//
+// e.g.
+// void deref_example(void* value) {
+// 	  int i = *((int*)value);
+// }
+//
+// can now be written as:
+//
+// void deref_example(void* value) {
+// 	  int i = DEREF_VOID(int, value);
+// }
+//===========================================================================
+#define DEREF_VOID(T, v)	\
+	*( (T*)( (v) ) );
 
 #endif // _UTILS_H_
