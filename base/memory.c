c265f6afe99bd265910f81c0f6728d5836b88a87
Yanzheng Li <yanzheng819@gmail.com>
2012-05-14 11:24:23 -0400

    base - memset16 and memset32 in base/memory.c

---
+++ base/memory.c
@@ -0,0 +1,17 @@
+void
+memset16(uint16_t *dst, uint16_t value, size_t size)
+{
+	size >>= 1;
+	while(size--) {
+		*dst++ = value;
+	}
+}
+
+void
+memset32(uint32_t *dst, uint32_t value, size_t size)
+{
+	size >>= 2;
+	while(size--) {
+		*dst++ = value;
+	}
+}
