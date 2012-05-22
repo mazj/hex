69516ef7e52bacf5e89249cc146f391ea798926c
Yanzheng Li <yanzheng819@gmail.com>
2012-05-22 11:59:28 -0400

    parser - added _check_type_qualifier_list

--- parser/astutils.c
+++ parser/astutils.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 static int
 _is_expr_list_identifier_list(ExprList* expr_list)
 {
@@ -20,4 +22,36 @@ _is_expr_list_identifier_list(ExprList* expr_list)
 	}
 
 	return 1;
+}
+
+static int
+_check_type_qualifier_list(TypeQualifierList *type_qualifier_list)
+{
+	assert(type_qualifier_list);
+
+	int type_qualifier_const_count = 0;
+	int type_qualifier_volatile_count = 0;
+	int type_qualifier_static_count = 0;
+
+	while(type_qualifier_list) {
+		int qualifier = type_qualifier_list->type_qualifier;
+
+		if(qualifier == type_qualifier_const)
+			type_qualifier_const_count++;
+		else if(qualifier == type_qualifier_volatile)
+			type_qualifier_volatile_count++;
+		else
+			type_qualifier_static_count++;
+
+		type_qualifier_list = type_qualifier_list->next;
+	}
+
+	if(type_qualifier_const_count > 1)
+		return 0;
+	else if(type_qualifier_volatile_count > 1)
+		return 0;
+	else if(type_qualifier_static_count > 1)
+		return 0;
+	else
+		return 1;
 }
