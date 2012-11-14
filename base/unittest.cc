e8b97a9c37d6edd88dfb34aa1208890209579b7f
Yanzheng Li <yanzheng819@gmail.com>
2012-11-14 02:14:15 -0800

    cleanup - code cleanup

--- base/unittest.cc
+++ base/unittest.cc
@@ -1,3 +1,27 @@
+/*
+ * HEX Programming Language
+ * Copyright (C) 2012  Yanzheng Li
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/* Unit test
+ *
+ * All unit testing stuff should be here.
+ * All unit tests do and should be linked to this.
+ * */
+
 #include "unittest.h"
 
 using ::testing::EmptyTestEventListener;
@@ -9,6 +33,8 @@ using ::testing::TestInfo;
 using ::testing::TestPartResult;
 using ::testing::UnitTest;
 
+
+/* entry point for unit test executable */
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
--- base/unittest.h
+++ base/unittest.h
@@ -1,4 +1,26 @@
-/* Unit test macros. */
+/*
+ * HEX Programming Language
+ * Copyright (C) 2012  Yanzheng Li
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/* Unit test
+ *
+ * All unit testing stuff should be here.
+ * All unit tests do and should include this header.
+ * */
 #ifndef _UNIT_TEST_H_
 #define _UNIT_TEST_H_
 
@@ -8,4 +30,4 @@
 
 
 
-#endif // _UNIT_TEST_H_
+#endif /* _UNIT_TEST_H_ */
